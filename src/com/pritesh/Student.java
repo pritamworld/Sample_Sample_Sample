801ee27efeb1c0d7ccc33293a8907a10cc1e3c45
moxDroid Labs Inc <pritam.world@gmail.com>
2021-02-27 09:49:06 -0500

    Student class updated with all attributes

--- src/com/pritesh/Student.java
+++ src/com/pritesh/Student.java
@@ -1,5 +1,22 @@
 package com.pritesh;
 
+import java.util.Date;
+
 public class Student
 {
+	private int studentId;
+	private String firstName;
+	private String lastName;
+	private Date birthDate;
+	private Gender gender; //Enum
+	private String address;
+	private String city;
+	private String pinCode;
+	private String cellPhone;
+	private String email;
+	private String department;
+	private float marks[];
+	private float totalMarks;
+	private float percentage;
+	private String grade; //A+, A
 }
