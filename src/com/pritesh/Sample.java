b6048286c7a23884119d7f2601943a346237bd3f
moxDroid Labs Inc <pritam.world@gmail.com>
2021-02-27 11:22:19 -0500

    Student objects created

--- src/com/pritesh/Sample.java
+++ src/com/pritesh/Sample.java
@@ -1,8 +1,49 @@
 package com.pritesh;
 
+import java.util.Date;
+
 public class Sample
 {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        /*Student s1 = new Student(1);
+        s1.setFirstName("Pritesh");
+        s1.setLastName("Patel");*/
+
+        float[] dalveerMarks = {80,80,80,80,80};
+        Student dalveer = new Student(789821,
+                "Dalveer",
+                "Singh",
+                new Date(),
+                Gender.MALE,
+                "String address",
+                "String city",
+                "String pinCode",
+                "String cellPhone",
+                "String email",
+                "String department",
+                dalveerMarks);
+        dalveer.calculateTotalMarks();
+        dalveer.calculatePercentage();
+        dalveer.calculateGrade();
+
+        float[] dwarkeshMarks = {90,90,90,90,90};
+        Student dwarkesh = new Student(789822,
+                "Dwarkesh",
+                "Singh",
+                new Date(),
+                Gender.MALE,
+                "String address",
+                "String city",
+                "String pinCode",
+                "String cellPhone",
+                "String email",
+                "String department",
+                dwarkeshMarks);
+        dwarkesh.calculateTotalMarks();
+        dwarkesh.calculatePercentage();
+        dwarkesh.calculateGrade();
+
+        System.out.println("Dalveer Grades: "+ dalveer.getGrade());
+        System.out.println("Dwarkesh Grades: "+dwarkesh.getGrade());
     }
 }
